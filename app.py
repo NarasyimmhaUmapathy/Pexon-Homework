309f134b2df1a252bc2c807caaca1c070d1ff62b
Fernando Zepeda <fernando.zepeda@xing.com>
2021-12-23 22:57:58 +0100

    try

--- app.py
+++ app.py
@@ -17,35 +17,40 @@ def db_connection():
 @app.route("/movies", methods=["GET", "POST"])
 def movies() -> wrappers.Response:
     
-    conn = db_connection()
-    cursor = conn.cursor()
-
-    if request.method == "GET":
-        cursor = conn.execute("SELECT * FROM movies")
-        books = [
-            dict(id=row[0], title=row[1],genre = [2], year = [3])
-            for row in cursor.fetchall()
-        ]
-
-        if movies is not None:
-            return jsonify(books) 
-
-    if request.method == "POST":
-        if request.json is not None:
-            # The data shall be defined as a list of objects
-            for obj in request.json:
-                new_title = obj["title"]
-                new_genre = obj["genre"]
-                year = obj["year"]
-                
-                # (Comments should go above the code) parametrized query
-                sql = """INSERT INTO movies (title, genre, year)
-                         VALUES (?,?,?)""" 
+    try:
+        conn = db_connection()
+    except Exception as e:
+        raise Exception("Not possible to establish a connection")
     
-                cursor = conn.execute(sql, (new_title, new_genre, year))
-            conn.commit()
+    if conn is not None:
+        cursor = conn.cursor()
+    
+        if request.method == "GET":
+            cursor = conn.execute("SELECT * FROM movies")
+            books = [
+                dict(id=row[0], title=row[1],genre = [2], year = [3])
+                for row in cursor.fetchall()
+            ]
+
+            if movies is not None:
+                return jsonify(books) 
+
+        if request.method == "POST":
+            if request.json is not None:
+                # The data shall be defined as a list of objects
+                for obj in request.json:
+                    new_title = obj["title"]
+                    new_genre = obj["genre"]
+                    year = obj["year"]
+                    
+                    # (Comments should go above the code) parametrized query
+                    sql = """INSERT INTO movies (title, genre, year)
+                            VALUES (?,?,?)""" 
         
-        # Better always return a JSON
-        return jsonify({"result":"Movies added successfully"})
+                    cursor = conn.execute(sql, (new_title, new_genre, year))
+                conn.commit()
+        
+            # Better always return a JSON
+            return jsonify({"result":"Movies added successfully"})
 
     return jsonify({"result": "Another response"})
